9657b2f6690214f3c40965a346e56152093827b7
karen09 <kazein09@gmail.com>
2014-11-26 12:27:04 +0100

    update

--- manifests/nodes/openduty.pp
+++ manifests/nodes/openduty.pp
@@ -1,34 +1,33 @@
 node 'openduty' {
-
   package {'openduty':
-    ensure  => '2.1-1',
+    ensure      => '2.1-1',
   }
 
-      yumrepo  {'openduty':
-             baseurl     => 'https://pulp.inuits.eu/upstream/',
-             gpgcheck    => '0',
-             enabled     => '1',
-      }
+  yumrepo  {'openduty':
+    baseurl     => 'https://pulp.inuits.eu/upstream/',
+    gpgcheck    => '0',
+    enabled     => '1',
+  }
 
-      exec     {'syncdb':
-             require     =>  Package['openduty'],
-             command     => 'source env/bin/activate && python manage.py syncdb && python manage.py migrate',
-             cwd         => '/opt/openduty/',
-             environment => 'DJANGO_SETTINGS_MODULE=openduty.settings_dev',
-             creates     => '/opt/openduty/database.sql',
-             path        => '/usr/bin',
-             provider    =>  shell,
-      }
+  exec     {'syncdb':
+    require     =>  Package['openduty'],
+    command     => 'source env/bin/activate && python manage.py syncdb && python manage.py migrate',
+    cwd         => '/opt/openduty/',
+    environment => 'DJANGO_SETTINGS_MODULE=openduty.settings_dev',
+    creates     => '/opt/openduty/database.sql',
+    path        => '/usr/bin',
+    provider    =>  shell,
+  }
 
-      service  {'openduty':
-             require     =>  Exec['syncdb'],
-             ensure      =>  running,
-      }
+  service  {'openduty':
+    require     =>  Exec['syncdb'],
+    ensure      =>  running,
+  }
 
-      firewall {'000 explicit rule Openduty-puppetized-':
-             proto       => 'tcp',
-             dport       => '8000',
-             action      => 'accept',
-      }
+  firewall {'000 explicit rule Openduty-puppetized-':
+    proto       => 'tcp',
+    dport       => '8000',
+    action      => 'accept',
+  }
 
 }
